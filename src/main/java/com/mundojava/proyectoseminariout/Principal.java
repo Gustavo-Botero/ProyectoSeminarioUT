253dc5029913424d06e86c80ceb364d174b4d653
Gustavo Botero <gustavoadolfob92@gmail.com>
2022-02-20 20:01:09 -0500

    Pantalla principal

--- src/main/java/com/mundojava/proyectoseminariout/Principal.java
+++ src/main/java/com/mundojava/proyectoseminariout/Principal.java
@@ -2,32 +2,37 @@ package com.mundojava.proyectoseminariout;
 
 import com.mundojava.proyectoseminariout.estado.EstadoDAO;
 import com.mundojava.proyectoseminariout.estado.EstadoDTO;
+import com.mundojava.proyectoseminariout.presentacion.menu_principal.VistaMenuPrincipal;
 import java.util.List;
 
 public class Principal {
     public static void main(String[] args) {
         
-        EstadoDAO estadoDAO = new EstadoDAO();
+        VistaMenuPrincipal vistaMenuPrincipal = new VistaMenuPrincipal();
+        
+        vistaMenuPrincipal.ejecutar();
+        
+        /*EstadoDAO estadoDAO = new EstadoDAO();
         
         List<EstadoDTO> estados = estadoDAO.listar();
         System.out.println("lista de estados");
         if(estados.size() > 0) {
-            for(EstadoDTO estado: estados) {
-                System.out.println("estado = " + estado);
-            }
-        } else {    
-            System.out.println("No hay estados registrados en el sistema");
+        for(EstadoDTO estado: estados) {
+        System.out.println("estado = " + estado);
+        }
+        } else {
+        System.out.println("No hay estados registrados en el sistema");
         }
         
         System.out.println("------------------------");
         
-        /*EstadoDTO estado = estadoDAO.consultarPorId(2);
+        EstadoDTO estado = estadoDAO.consultarPorId(2);
         System.out.println(estado);
         
         List<EstadoDTO> nombre = estadoDAO.consultarPorNombre("pago");
-        System.out.println(nombre);*/
+        System.out.println(nombre);
         
-        /*EstadoDTO nuevoEstado = new EstadoDTO(3,"PENDIENTE DE FALLO", 0);
+        EstadoDTO nuevoEstado = new EstadoDTO(3,"PENDIENTE DE FALLO", 0);
         
         int insertado = estadoDAO.adicionar(nuevoEstado);
         
@@ -35,16 +40,16 @@ public class Principal {
         System.out.println("Dato insertado correctamente");
         } else {
         System.out.println("Dato no pudo ser insertado");
-        }*/
+        }
         
-        /*int eliminar = estadoDAO.eliminar(3);
+        int eliminar = estadoDAO.eliminar(3);
         System.out.println("eliminar = " + eliminar);
         
         if (eliminar > 0) {
         System.out.println("Dato eliminado correctamente");
         } else {
         System.out.println("Dato no pudo ser eliminado");
-        }*/
+        }
         
         EstadoDTO modificandoEstado = new EstadoDTO(0,"PENDIENTE", 0);
         int modificar = estadoDAO.modificar(2, modificandoEstado);
@@ -54,6 +59,6 @@ public class Principal {
         } else {
             System.out.println("El dato no se pudo modificar");
         }
-        System.out.println("modificar = " + modificar);
+        System.out.println("modificar = " + modificar);*/
     }
 }
